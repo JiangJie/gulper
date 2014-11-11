0caeae594bbb355794fbdc81b4bd2f7e3c55ecb8
Jarvis Jiang <jiang115jie@gmail.com>
2014-11-11 17:14:55 +0800

    add gulp zip task

--- gulpfile.js
+++ gulpfile.js
@@ -18,20 +18,42 @@ var nib = require('nib');
 var usemin = require('gulp-usemin');
 var revall = require('gulp-rev-all');
 
-var cdn = require('gulp-cdn-replace');
+var cdnReplace = require('gulp-cdn-replace');
 
 var sprite = require('css-sprite').stream;
 
+var zip = require('gulp-zip');
+
 var run = require('run-sequence');
 
+var project = require('./project');
+
 var src = './src';
-var dest = './dist';
+var dest = {
+    root: './public',
+    webserver: './public/webserver',
+    cdn: './public/cdn',
+    offline: './public/offline'
+};
+var offline = './offline';
+var cdn = {
+    js: typeof project.cdn === 'string' ? project.cdn : (project.cdn.js || ''),
+    css: typeof project.cdn === 'string' ? project.cdn : (project.cdn.css || '')
+};
+
+function getDirFromUrl(url) {
+    url = url.split('//');
+    return url[url.length - 1];
+}
 
 gulp.task('clean', function(fn) {
-    del(dest, fn);
+    del(dest.root, fn);
 });
 gulp.task('clean-dev', function(fn) {
     del([src + '/js', src + '/css'], fn);
+});
+gulp.task('clean-offline', function(fn) {
+    del(offline, fn);
 });
 
 gulp.task('tmpl', function() {
@@ -66,48 +88,47 @@ gulp.task('img', function() {
         .pipe(gulp.dest(src + '/css/img/'));
 });
 
-gulp.task('usemin', ['clean'], function() {
-    gulp.src(src + '/*.html')
-        .pipe(usemin({
-            zepto: [revall({hashLength: 5})],
-            index_js: [
-                revall({
-                    // prefix: 'http://s1.url.cn/qqweb/m/gulper/',
-                    hashLength: 5
-                })
-            ],
-            index_css: [
-                revall({
-                    hashLength: 5
-                })
-            ]
-        }))
-        .pipe(gulp.dest(dest));
-});
+// gulp.task('usemin', ['clean'], function() {
+//     gulp.src(src + '/*.html')
+//         .pipe(usemin({
+//             zepto: [revall({hashLength: 5})],
+//             index_js: [
+//                 revall({
+//                     // prefix: 'http://s1.url.cn/qqweb/m/gulper/',
+//                     hashLength: 5
+//                 })
+//             ],
+//             index_css: [
+//                 revall({
+//                     hashLength: 5
+//                 })
+//             ]
+//         }))
+//         .pipe(gulp.dest(dest));
+// });
 
-gulp.task('revall', ['clean'], function() {
-    gulp.src(src+ '/js/**')
+gulp.task('rev-js', function() {
+    return gulp.src(src+ '/js/**')
         .pipe(revall({
             hashLength: 5
         }))
-        .pipe(gulp.dest(dest + '/js/'));
-    gulp.src(src+ '/css/**')
+        .pipe(gulp.dest(dest.cdn + '/js/'));
+});
+gulp.task('rev-css', function() {
+    return gulp.src(src+ '/css/**')
         .pipe(revall({
             hashLength: 5
         }))
-        .pipe(gulp.dest(dest + '/css/'));
+        .pipe(gulp.dest(dest.cdn + '/css/'));
 });
 
 gulp.task('cdn', function() {
-    gulp.src(src + '/*.html')
-        .pipe(cdn({
-            dir: dest,
-            root: {
-                js: 'http://s1.url.cn/gulper/',
-                css: 'http://s2.url.cn/gulper/'
-            }
+    return gulp.src(src + '/*.html')
+        .pipe(cdnReplace({
+            dir: dest.cdn,
+            root: cdn
         }))
-        .pipe(gulp.dest(dest));
+        .pipe(gulp.dest(dest.webserver));
 });
 
 gulp.task('sprite', function() {
@@ -125,4 +146,30 @@ gulp.task('sprite', function() {
 gulp.task('default', ['clean-dev'], function() {
     run(['jslib', 'browserify']);
     run('sprite', ['stylus', 'img']);
+});
+
+gulp.task('dist', ['clean'], function() {
+    run(['rev-js', 'rev-css'], 'cdn');
+});
+
+gulp.task('zip-html', function() {
+    return gulp.src(dest.webserver + '/**')
+        .pipe(gulp.dest(offline + '/' + getDirFromUrl(project.webServer)));
+});
+gulp.task('zip-js', function() {
+    return gulp.src(dest.cdn + '/js/**')
+        .pipe(gulp.dest(offline + '/' + getDirFromUrl(cdn.js)));
+});
+gulp.task('zip-css', function() {
+    return gulp.src(dest.cdn + '/css/**')
+        .pipe(gulp.dest(offline + '/' + getDirFromUrl(cdn.css)));
+});
+gulp.task('zip-local', function() {
+    return gulp.src(offline + '/**')
+        .pipe(zip('local.zip'))
+        .pipe(gulp.dest(dest.offline));
+});
+
+gulp.task('zip', ['clean-offline'], function() {
+    run(['zip-html', 'zip-js', 'zip-css'], 'zip-local', 'clean-offline');
 });
